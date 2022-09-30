12c3537b85a1adb6e277efd6f834edbc156acf18
Vic Siu <ysiu4@my.centennialcollege.ca>
2022-09-30 14:10:39 -0400

    finished projects page

--- routes/projects.js
+++ routes/projects.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('about', {title: "My Projects"});
+  res.render('projects', {
+    title: "My Projects",
+    my_info: MyInfo,
+  });
 });
 
 module.exports = router;
